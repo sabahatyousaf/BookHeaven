ae86175e2737e453fb617104366f6ade5084dc43
sabahatyousaf <sabahatyousaf062@gmail.com>
2026-02-12 11:33:56 +0500

    backend deployed on vercel

--- book_heaven_user/src/redux/config/Config.jsx
+++ book_heaven_user/src/redux/config/Config.jsx
@@ -1,7 +1,7 @@
 const CONFIG = {
   // BASE_URL: 'http://10.0.2.2:8000/api',
-  BASE_URL: 'http://192.168.1.15:8000/api',
-  // BASE_URL: 'https://book-heaven-backend-deployment.onrender.com/api',
+  // BASE_URL: 'http://192.168.1.15:8000/api',
+  BASE_URL: 'https://book-heaven-backend-plum.vercel.app/api',
 };
 
 export default CONFIG;
